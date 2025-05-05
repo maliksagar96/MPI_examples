2044c2259dfebf48ee751b7e71f006995853d07c
maliksagar96 <sagar@DESKTOP-758TFAQ>
2025-05-05 17:38:16 +0530

    Parallel Gauss Jordan

--- GaussElimination/GE.cpp
+++ GaussElimination/GE.cpp
@@ -21,7 +21,8 @@ int main(int argc, char* argv[]) {
   //   }
 
   // float matrix1[N][N+1] = {{1,3,6,25}, {2,7,14,58}, {0,2,5,19}};
-  float matrix1[N][N+1] = {{1,1,-1,-2}, {2,-1,1,5}, {-1,2,2,1}};
+  // float matrix1[N][N+1] = {{1,1,-1,-2}, {2,-1,1,5}, {-1,2,2,1}};
+  float matrix1[N][N+1] = {{2,1,2,10}, {1,2,1,8}, {3,1,-1,2}};
 
   //Gauss Elimination
   for(int first_ptr = 0;first_ptr<N-1;first_ptr++) {
@@ -32,14 +33,18 @@ int main(int argc, char* argv[]) {
     }
   }
 
-  //Gauss Jordan 
-
+  //Gauss Jordan
   for(int first_ptr = N-1;first_ptr>=0;first_ptr--) {
     for(int second_ptr = first_ptr-1;second_ptr>=0;second_ptr--) {
       for(int column = N;column>=0;column--) {      
-        matrix1[second_ptr][column] -=  (matrix1[second_ptr][first_ptr]/matrix1[first_ptr][first_ptr]) *  matrix1[first_ptr][column];
+        matrix1[second_ptr][column] -= (matrix1[second_ptr][first_ptr]/matrix1[first_ptr][first_ptr]) * matrix1[first_ptr][column];
       }
     }
+  }
+
+  for(int i = 0;i<N;i++) {
+    matrix1[i][N] /= matrix1[i][i];
+    matrix1[i][i] = 1;
   }
 
 
